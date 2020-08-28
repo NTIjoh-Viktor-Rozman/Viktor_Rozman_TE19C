aeabb5560bfcc966c3dbb0a81aa1d74e97d7db1d
Vikstarainia <70264443+Vikstarainia@users.noreply.github.com>
2020-08-28 09:37:29 +0200

    Introduktionskod

--- README.md
+++
@@ -1,2 +0,0 @@
-# Viktor_Rozman_TE19C
-Jag gillar Bleb för Bleb är Bleb och jag gillar Bleb!!
---
+++ syntax.py
@@ -0,0 +1,11 @@
+print("hej TE19C")
+
+Name = "Viktor"
+
+print(f"Ditt namn är {Name}")
+print("Ditt namn är",Name)
+
+side = float(input("Ange kvadratens sida: "))
+area = side**2
+
+print(f"Arean av kvadraten är {area} a.e.")
